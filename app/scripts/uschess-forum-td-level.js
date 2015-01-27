5e12bb1db0d40a8723db30cee954108364e37d75
Vincent Simard <vincent@vincentsimard.com>
2015-01-27 00:37:42 -0500

    Creating uscf profile link

--- app/scripts/uschess-forum-td-level.js
+++ app/scripts/uschess-forum-td-level.js
@@ -3,24 +3,38 @@
 var forEach = Array.prototype.forEach;
 var postProfiles = document.querySelectorAll('.post .postprofile');
 
-var getUSCFId = function(profile) {
-  var isUSCFIdNode = function(item) {
-    return item.textContent.indexOf('USCFId') > -1;
-  };
-
-  var dds = profile.querySelectorAll('dd');
-  var ddsArray = Array.prototype.slice.call(dds);
-
-  var idDD = ddsArray.filter(isUSCFIdNode);
-
-  console.log(idDD[0] && idDD[0].textContent.replace('USCFId: ', ''));
+var isUSCFIdNode = function(item) {
+  return item.textContent.indexOf('USCFId') > -1;
 };
 
 var processProfile = function(profile) {
-  var id = getUSCFId(profile);
+  var ddsArray = Array.prototype.slice.call(profile.querySelectorAll('dd'));
+  var idNode = ddsArray.filter(isUSCFIdNode)[0];
+  var id = idNode.textContent.replace('USCFId: ', '');
 
-  // console.log(profile);
+  wrapElement(idNode, id);
+};
+
+var wrapElement = function(node, id) {
+  var createUSCFLink = function(node, id) {
+    var link = document.createElement('a');
+    link.href = 'http://www.uschess.org/msa/MbrDtlMain.php?' + id;
+    link.appendChild(node);
+
+    return link;
+  };
+  
+  var parent = node.parentNode;
+
+  var position = 0;
+  for(var i = 0; i<parent.childNodes.length; i++) {
+    if(parent.childNodes[i] === node) {
+      position = i;
+      break;
+    };
+  };
+
+  parent.insertBefore(createUSCFLink(node, id), parent.childNodes[position]);
 };
 
 forEach.call(postProfiles, processProfile);
-
