825df5f2d0dca728096748e2c0bd395ec8390ba8
Vincent Simard <vincent@vincentsimard.com>
2015-01-27 00:03:45 -0500

    Selecting USCFIds

--- app/scripts/uschess-forum-td-level.js
+++ app/scripts/uschess-forum-td-level.js
@@ -3,6 +3,24 @@
 var forEach = Array.prototype.forEach;
 var postProfiles = document.querySelectorAll('.post .postprofile');
 
-forEach.call(postProfiles, function(profile) {
-  console.log(profile);
-});
+var getUSCFId = function(profile) {
+  var isUSCFIdNode = function(item) {
+    return item.textContent.indexOf('USCFId') > -1;
+  };
+
+  var dds = profile.querySelectorAll('dd');
+  var ddsArray = Array.prototype.slice.call(dds);
+
+  var idDD = ddsArray.filter(isUSCFIdNode);
+
+  console.log(idDD[0] && idDD[0].textContent.replace('USCFId: ', ''));
+};
+
+var processProfile = function(profile) {
+  var id = getUSCFId(profile);
+
+  // console.log(profile);
+};
+
+forEach.call(postProfiles, processProfile);
+
